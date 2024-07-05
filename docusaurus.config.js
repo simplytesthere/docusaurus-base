d71b09be91ff235a263837da73aa8033491c9d3f
simplytesthere <sye0williams@gmail.com>
2024-07-05 12:46:30 -0400

    site changes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -66,17 +66,17 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'ASC Docs Site',
         logo: {
           alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          src: 'img/homepagebutton.svg',
         },
         items: [
           {
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Documentation',
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
